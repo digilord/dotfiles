673d75d561ee723604155835d6e78addfbffd280
Daniel Morrigan <digilord@me.com>
2019-09-14 08:56:26 -0700

    Working through initial setup STILL

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -26,10 +26,10 @@ function reload_config(files)
   hs.reload()
 end
 
--- hyper:bind({}, "r", function()
---   reload_config()
---   hyper.triggered = true
--- end)
+hyper:bind({}, "r", function()
+  reload_config()
+  hyper.triggered = true
+end)
 
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
 hs.alert.show("Config loaded")
