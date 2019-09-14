7764e6f8e46ab355741e36c25ce24b92a5b4b733
Daniel Morrigan <digilord@me.com>
2019-09-14 11:25:32 -0700

    SWEEPING changes

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,233 +1,57 @@
-require "hyper"
-require "caffeinate"
-require "minimising"
-require "togglevpn"
-require "spectacle"
-require "shortcuts"
-require "bluetooth"
-require "toggle_input_volume"
-require "countdown"
-require "mirowindowsmanager"
--- hs.window.filter.ignoreAlways['Sourcetree Networking'] = true
--- hs.window.filter.ignoreAlways['iTunes'] = true
--- hs.window.filter.ignoreAlways['Finder'] = true
+hyper = {"ctrl", "alt", "cmd", "shift"}
 
-hs.logger.defaultLogLevel="info"
+-- A global variable for the Hyper Mode
+k = hs.hotkey.modal.new({}, "F17")
 
-hyper = {"cmd","alt","ctrl"}
-shift_hyper = {"cmd","alt","ctrl","shift"}
+lfun0 = function()
+    hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+end
+k:bind('', 'v', nil, lfun0)
 
-col = hs.drawing.color.x11
+-- HYPER+L: Open news.google.com in the default browser
+lfun = function()
+  news = "app = Application.currentApplication(); app.includeStandardAdditions = true; app.doShellScript('open http://news.google.com')"
+  hs.osascript.javascript(news)
+  k.triggered = true
+end
+k:bind('', 'l', nil, lfun)
 
------------------------------------------------
--- Reload config on write
------------------------------------------------
-function reload_config(files)
-  hs.reload()
+-- HYPER+M: Call a pre-defined trigger in Alfred 3
+-- mfun = function()
+--   cmd = "tell application \"Alfred 4\" to run trigger \"emoj\" in workflow \"com.sindresorhus.emoj\" with argument \"\""
+--   hs.osascript.applescript(cmd)
+--   k.triggered = true
+-- end
+-- k:bind({}, 'm', nil, mfun)
+
+-- HYPER+E: Act like ⌃e and move to end of line.
+efun = function()
+  hs.eventtap.keyStroke({'⌃'}, 'e')
+  k.triggered = true
+end
+k:bind({}, 'e', nil, efun)
+
+-- HYPER+A: Act like ⌃a and move to beginning of line.
+afun = function()
+  hs.eventtap.keyStroke({'⌃'}, 'a')
+  k.triggered = true
+end
+k:bind({}, 'a', nil, afun)
+
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+pressedF18 = function()
+  k.triggered = false
+  k:enter()
 end
 
-hyper:bind({}, "r", function()
-  reload_config()
-  hyper.triggered = true
-end)
-
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Config loaded")
-
-hs.application.enableSpotlightForNameSearches(true)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x - 10
-  f.y = f.y - 10
-  win:setFrame(f)
-end)
-
--- local hyper = {"ctrl", "alt", "cmd"}
--- hs.MiroWindowsManager:bindHotkeys({
---   up = {hyper, "up"},
---   right = {hyper, "right"},
---   down = {hyper, "down"},
---   left = {hyper, "left"},
---   fullscreen = {hyper, "f"}
--- })
-
-
--- digilord
--- Movement using the nethack
--- y   k   u
--- h       l
--- b   j   n
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Y", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x - 10
-  f.y = f.y - 10
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.y = f.y - 10
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x + 10
-  f.y = f.y - 10
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x - 10
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x + 10
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "B", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x - 10
-  f.y = f.y + 10
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.y = f.y + 10
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  f.x = f.x + 10
-  f.y = f.y + 10
-  win:setFrame(f)
-end)
-
--- Upper  -  ⌘+⌥+ctrl+←
--- Lower  -  ⌘+⌥+ctrl+←
--- Left   -  ⌘+⌥+ctrl+←
--- Right  -  ⌘+⌥+ctrl+←
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y + (max.h / 2)
-  f.w = max.w
-  f.h = max.h / 2
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h / 2
-  win:setFrame(f)
-end)
-
--- LAYOUT! Putting the window manager to work.
-local laptopScreen = "Color LCD"
-local windowLayout = {
-    {"Safari",  nil,          laptopScreen, hs.layout.left50,    nil, nil},
-    {"Mail",    nil,          laptopScreen, hs.layout.right50,   nil, nil},
-    -- {"iTunes",  "iTunes",     laptopScreen, hs.layout.maximized, nil, nil},
-    -- {"iTunes",  "MiniPlayer", laptopScreen, nil, nil, hs.geometry.rect(0, -48, 400, 48)},
-}
-hs.layout.apply(windowLayout)
-
-local function cleanPasteboard()
-  local pb = hs.pasteboard.contentTypes()
-  local contains = hs.fnutils.contains
-  if contains(pb, "com.apple.webarchive") and contains(pb, "public.rtf") then
-    hs.pasteboard.setContents(hs.pasteboard.getContents())
+-- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
+--   send ESCAPE if no other keys are pressed.
+releasedF18 = function()
+  k:exit()
+  if not k.triggered then
+    hs.eventtap.keyStroke({}, 'ESCAPE')
   end
 end
 
-local messagesWindowFilter = hs.window.filter.new(false):setAppFilter('Messages')
-messagesWindowFilter:subscribe(hs.window.filter.windowFocused, cleanPasteboard)
-
-function reloadConfig(files)
-  doReload = false
-  for _,file in pairs(files) do
-      if file:sub(-4) == ".lua" then
-          doReload = true
-      end
-  end
-  if doReload then
-      hs.reload()
-  end
-end
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
-
-
--- Added by digilord
-hs.loadSpoon("ReloadConfiguration")
-spoon.ReloadConfiguration:start()
-
--- require("bin.hammerspoon.txt.json")
+-- Bind the Hyper key
+f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
